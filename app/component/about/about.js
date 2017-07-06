a0d94f341cb97a0d6ff19a3fbcdcc85ad552e210
Bhavya <bhavyab@outlook.com>
2017-07-06 12:23:46 -0700

    sos

--- app/component/about/about.js
+++ app/component/about/about.js
@@ -8,5 +8,5 @@ module.exports = {
 
 function AboutController($log, $location) {
   $log.debug('AboutController');
-  this.image = 'app/assets/bhavya2.jpg' || 'http://www.cliparts101.com/files/4/EE434F2FDC670AB3C2E50DF1DA3DCEB5/Brush_B.png';
+  this.image = 'app/assets/bhavya2.jpg';
 }
