a69c114d6f832d795015fced93e5e27dfa0cf31a
Bhavya <bhavyab@outlook.com>
2017-07-13 15:21:43 -0700

    log turned off

--- app/service/email-service.js
+++ app/service/email-service.js
@@ -6,7 +6,7 @@ function emailService($q, $log, $http){
   $log.debug('emailService');
 
   let service = {};
-  let url = `${__API_URL__}/contact`;
+  let url = `${__API_URL__}contact`;
   let config = {
     headers: {
       'Content-Type': 'application/json',
@@ -23,9 +23,9 @@ function emailService($q, $log, $http){
       data: JSON.stringify(form),
       config: config
     }).then(function successCallback(response) {
-      console.log('email send -- ',response, 'response data -- '+ response.data);
+      $log.debug('email send -- ',response, 'response data -- '+ response.data.responseCode);
     }, function errorCallback(response) {
-      console.log('error occured -- ', response);
+      $log.debug('error occured -- ', response);
     });
   };
   return service;
