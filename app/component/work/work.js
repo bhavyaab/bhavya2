3f71c7ab2981ae672325418eba62638318569c96
Bhavya <bhavyab@outlook.com>
2017-07-13 13:21:26 -0700

    project moved down

--- app/component/work/work.js
+++ app/component/work/work.js
@@ -9,6 +9,47 @@ module.exports = {
 function WorkController($log, $location, $element) {
   $log.debug('WorkController');
 
+
+  this.projects = [
+    {
+      name: 'Inbox Health',
+      url: 'http://inbox-health.herokuapp.com/',
+      github: 'https://github.com/bhavyaab/inbox-health',
+      button: 'Visit site',
+      description: 'This web app lets you instantly see a list of all your subscription emails. Unsubscribe and delete easily from whatever you don’t want.',
+      technical: 'Gmail api, jquery, Sql, Handelbar templates, MVC',
+      // image: './assets/inbox-health.svg'
+      image: 'http://inbox-health.herokuapp.com/img/fullLogo.svg'
+    },
+    {
+      name: 'wheatlessly',
+      url: 'http://www.wheatlessly.com/ ',
+      github: 'https://github.com/jmpaik/wheatlessly',
+      button: 'Visit site',
+      description: ' Wheatlessly, is a dedicated gluten free web and mobile application that allows users to signin/signout and post their business. It also uses Google geocoder API to locate businesses.',
+      technical: 'NodeJs, Angular, Sass, webpack, Karma,',
+      image: './assets/wheatlessly.jpg'
+    },
+    {
+      name: 'wheatlessIn',
+      url: 'https://github.com/jmpaik/wheatlessinv2',
+      github: 'https://github.com/jmpaik/wheatlessinv2',
+      button: 'Visit DOC',
+      description: 'It is web service serving Wheatlessly app, built using node.js, angularJS and expressJS server, uses MongoDB and AWS S3 database.',
+      technical: 'REST API node.js, angularJS expressJS MongoDB and AWS S3 database, trivas CI.',
+      image: './assets/backend.jpg'
+    },
+    {
+      name: 'Portfolio',
+      url: 'https://bhavya2.herokuapp.com/',
+      github: 'https://github.com/bhavyaab/bhavya2',
+      button: 'Visit site',
+      description: 'It is a front end portfolio and deployed on heroku.',
+      technical: 'Bootstrap, node.js, angularJS, webpack',
+      image: 'https://s-media-cache-ak0.pinimg.com/736x/f6/eb/63/f6eb6350bf4032462e74d29f19f4594d.jpg'
+    }
+  ];
+
   var width = $element[0].childNodes[0].childNodes[1].childNodes[1].offsetWidth * 0.9;
   this.angle = 360/(this.projects.length);
   this.zindex = width + 'px' || '280px';
@@ -29,48 +70,5 @@ function WorkController($log, $location, $element) {
     }
 
     document.getElementsByClassName('carousel')[0].style.transform = 'rotateY(' + currAngle + 'deg)';
-
   };
-
-
-  this.projects = [
-    {
-      name: 'Inbox Health',
-      url: 'http://inbox-health.herokuapp.com/',
-      github: 'https://github.com/bhavyaab/inbox-health',
-      button: 'Visit site',
-      description: 'This web app lets you instantly see a list of all your subscription emails. Unsubscribe and delete easily from whatever you don’t want.',
-      technical: 'Gmail api, jquery, Sql, Handelbar templates, MVC',
-      // image: './assets/inbox-health.svg'
-      image: 'http://inbox-health.herokuapp.com/img/fullLogo.svg'
-    },
-    {
-      name: 'wheatlessly',
-      url: 'http://www.wheatlessly.com/ ',
-      github: 'https://github.com/jmpaik/wheatlessly',
-      button: 'Visit site',
-      description: ' Wheatlessly, is a dedicated gluten free web and mobile application that allows users to signin/signout and post their business. It also uses Google geocoder API to locate businesses.',
-      technical: 'NodeJs, Angular, Sass, webpack, Karma,',
-      image: './app/assets/wheatlessly.jpg'
-    },
-    {
-      name: 'wheatlessIn',
-      url: 'https://github.com/jmpaik/wheatlessinv2',
-      github: 'https://github.com/jmpaik/wheatlessinv2',
-      button: 'Visit DOC',
-      description: 'It is web service serving Wheatlessly app, built using node.js, angularJS and expressJS server, uses MongoDB and AWS S3 database.',
-      technical: 'REST API node.js, angularJS expressJS MongoDB and AWS S3 database, trivas CI.',
-      image: './app/assets/backend.jpg'
-    },
-    {
-      name: 'Portfolio',
-      url: 'https://bhavyab.herokuapp.com/',
-      github: 'https://github.com/bhavyaab/bhavya',
-      button: 'Visit site',
-      description: 'It is a front end portfolio and deployed on heroku.',
-      technical: 'Bootstrap, node.js, angularJS, webpack',
-      image: 'https://s-media-cache-ak0.pinimg.com/736x/f6/eb/63/f6eb6350bf4032462e74d29f19f4594d.jpg'
-    }
-  ];
-
 }
