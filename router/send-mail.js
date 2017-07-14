526a3764cb33cb80b281ec28d4a7500ea4c4d76e
Bhavya <bhavyab@outlook.com>
2017-07-14 15:21:28 -0700

    ifttt setup

--- router/send-mail.js
+++ router/send-mail.js
@@ -4,9 +4,11 @@
 const jsonParser = require('body-parser').json();
 const Router = require('express').Router;
 const sendMail = module.exports = new Router();
-
 const dotenv = require('dotenv').load();
-var IFTTTMaker = require('iftttmaker')(process.env.IFTTT_KEY);
+
+
+var apiKey = process.env.IFTTT_KEY;
+var IFTTTMaker = require('iftttmaker')(apiKey);
 
 sendMail.post('/contact', jsonParser, function(req, res) {
   var data = req.body;
@@ -14,13 +16,19 @@ sendMail.post('/contact', jsonParser, function(req, res) {
   var request = {
     event: 'portfolio_contact',
     values: {
-      value1: data.name || 'Name not shared!',
-      value2: data.email || 'Contact not shared!',
+      value1: data.email || 'Contact not shared!',
+      value2: data.name || 'Name not shared!',
       value3: data.message || 'No message'
     }
   };
 
-  IFTTTMaker.send(request)
+  IFTTTMaker.send(request, function (error) {
+    if (error) {
+      console.log('The request could not be sent:', error);
+    } else {
+      console.log('Request was sent');
+    }
+  })
   .then(data => res.send(data))
   .catch(err => res.err(err));
 
