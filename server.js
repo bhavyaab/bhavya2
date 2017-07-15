23f6ff30837c09c5da0e9b087e30e45016c3f148
Bhavya Bharti <bhavyab@outlook.com>
2017-07-15 02:55:56 -0700

    res.header

--- server.js
+++ server.js
@@ -11,7 +11,12 @@ const sendMail = require('./router/send-mail.js');
 
 app.use(sendMail);
 app.use(express.static(`${__dirname}/build`));
-
+app.all('*', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 app.listen(PORT, function(){
   console.log('server up:', PORT);
 });
