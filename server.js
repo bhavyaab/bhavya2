e3c283bd0c60f256dce07b0e7e361177411c7bd0
Bhavya Bharti <bhavyab@outlook.com>
2017-07-15 03:21:42 -0700

    log response

--- server.js
+++ server.js
@@ -14,9 +14,12 @@ app.use(sendMail);
 app.use(express.static(`${__dirname}/build`));
 
 app.get('/', function (req, res, next) {
+  console.log('header setting');
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+  console.log('res.header ', res.header);
   next();
+
 });
 
 app.listen(PORT, function(){
