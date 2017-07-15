60ae5d73049bcbc942b588945e2163e2f4b80920
Bhavya Bharti <bhavyab@outlook.com>
2017-07-15 03:01:30 -0700

    cccccccccccccc

--- server.js
+++ server.js
@@ -2,21 +2,17 @@
 
 const express = require('express');
 const dotenv = require('dotenv').load();
-
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 const sendMail = require('./router/send-mail.js');
 
+app.use(cors());
 
 app.use(sendMail);
 app.use(express.static(`${__dirname}/build`));
-app.all('*', function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+
 app.listen(PORT, function(){
   console.log('server up:', PORT);
 });
