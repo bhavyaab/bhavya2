3ca60408e5dec11ef221b8e40531753496447812
Bhavya Bharti <bhavyab@outlook.com>
2017-07-15 01:29:03 -0700

    message works fine

--- app/component/work/work.js
+++ app/component/work/work.js
@@ -53,7 +53,6 @@ function WorkController($log, $location, $element) {
   var width = $element[0].childNodes[0].childNodes[1].childNodes[1].offsetWidth * 0.6;
   this.angle = 360/(this.projects.length);
   this.zindex = width + 'px' || '280px';
-  console.log('width -  ' ,$element[0].childNodes[0].childNodes[1].childNodes[1].offsetWidth, 'this.zindex ==',this.zindex);
 
   this.transform = function(i){
     return {'transform': 'rotateY(' + i * this.angle + 'deg) translateZ(' + this.zindex + ')',
