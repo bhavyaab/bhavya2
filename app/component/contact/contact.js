d39bbb41eac5d2a408f9449663acc2a432f04841
Bhavya <bhavyab@outlook.com>
2017-07-14 17:27:51 -0700

    comtact thanks

--- app/component/contact/contact.js
+++ app/component/contact/contact.js
@@ -8,18 +8,24 @@ module.exports = {
 
 function ContactController($log, $location, emailService) {
   $log.debug('ContactController');
+  this.hello = true;
+  this.placeholder = {
+    name: 'Your name ..',
+    email: 'Your email..',
+    message: 'Write something..'
 
-  this.messagePlaceholder = 'Write something..';
+  };
   this.form = {};
 
   this.email = function(){
     if(!this.form.name) $log.debug('name not found!');
-    if(!this.form.email) $log.debug('email not found!');
+    if(!this.form.email) this.placeholder.email = 'How I can Contact You?';
     if(!this.form.message) {
-      this.messagePlaceholder = 'Did you forget to write something!!';
+      this.placeholder.message = 'Did you forget to write something!!';
     } else {
       emailService.sendMail(this.form)
-      .then(this.form = {});
+      .then(this.form = {})
+      .then(this.hello = false);
     }
   };
 }
