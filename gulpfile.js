0e1266aa7e71124eec17e5253cead480f821a19d
Bhavya Bharti <bhavyab@outlook.com>
2017-07-15 03:15:24 -0700

    gulf off

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,22 @@
-var gulp = require('gulp');
-var gutil = require('gulp-util');
-var webpack = require('webpack');
-var WebpackDevServer = require('webpack-dev-server');
-var webpackConfig = require('./webpack.config.js');
-const sendMail = require('./router/send-mail.js');
-
-const PORT = process.env.PORT || 3000;
-
-gulp.task('default', ['webpack-dev-server']);
-
-gulp.task('webpack-dev-server', function(callback) {
-	// Start a webpack-dev-server
-  new WebpackDevServer(webpack(webpackConfig), {
-    stats: {
-      colors: true
-    }
-  }).listen(PORT, 'localhost', function(err) {
-    if(err) throw new gutil.PluginError('webpack-dev-server', err);
-    gutil.log('[webpack-dev-server]', 'http://localhost:' + PORT + '/webpack-dev-server/index.html');
-  });
-});
+// var gulp = require('gulp');
+// var gutil = require('gulp-util');
+// var webpack = require('webpack');
+// var WebpackDevServer = require('webpack-dev-server');
+// var webpackConfig = require('./webpack.config.js');
+// const sendMail = require('./router/send-mail.js');
+//
+// const PORT = process.env.PORT || 3000;
+//
+// gulp.task('default', ['webpack-dev-server']);
+//
+// gulp.task('webpack-dev-server', function(callback) {
+// 	// Start a webpack-dev-server
+//   new WebpackDevServer(webpack(webpackConfig), {
+//     stats: {
+//       colors: true
+//     }
+//   }).listen(PORT, 'localhost', function(err) {
+//     if(err) throw new gutil.PluginError('webpack-dev-server', err);
+//     gutil.log('[webpack-dev-server]', 'http://localhost:' + PORT + '/webpack-dev-server/index.html');
+//   });
+// });
--- server.js
+++ server.js
@@ -2,16 +2,22 @@
 
 const express = require('express');
 const dotenv = require('dotenv').load();
-const cors = require('cors');
+// const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 const sendMail = require('./router/send-mail.js');
 
-app.use(cors());
+// app.use(cors());
 
 app.use(sendMail);
 app.use(express.static(`${__dirname}/build`));
+
+app.get('/', function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+  next();
+});
 
 app.listen(PORT, function(){
   console.log('server up:', PORT);
