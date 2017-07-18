e179a06e0f5808681a7d20af479c0a85a9176424
Bhavya <bhavyab@outlook.com>
2017-07-18 14:07:56 -0700

    scrollTotop button show and hide works fine

--- app/view/home/home-controller.js
+++ app/view/home/home-controller.js
@@ -2,9 +2,9 @@
 
 require('./_home.scss');
 
-module.exports = ['$log', '$location', '$document', homeController];
+module.exports = ['$log', '$location', '$document', '$window', homeController];
 
-function homeController($log, $location, $document){
+function homeController($log, $location, $document, $window){
   $log.debug('homeController');
 
   let url = $location.url();
@@ -14,4 +14,16 @@ function homeController($log, $location, $document){
     $document.scrollTop(30, 3000);
   };
 
+  function showOnScroll(ev){
+    // $log.debug('yoffset - ', $window.pageYOffset, $window.innerHeight);
+    if(window.pageYOffset>950) {
+      // $log.debug('winow scrolled to 950');
+      document.getElementById('showTopScoller').style.display = 'block';
+    }
+    else {
+      document.getElementById('showTopScoller').style.display = 'none';
+      // $log.debug('not scrolled yet');
+    }
+  }
+  window.onscroll=showOnScroll;
 }
