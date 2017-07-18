27f195a06fbdc1ddf2e046a61d895c3781fa304f
Bhavya <bhavyab@outlook.com>
2017-07-18 12:14:26 -0700

    ham burger added

--- app/component/tabs/tabs.js
+++ app/component/tabs/tabs.js
@@ -2,23 +2,23 @@
 
 module.exports = {
   template: require('./tabs.html'),
-  controller: ['$log', '$location', '$document', TabsController],
+  controller: ['$log', '$location', '$document','$window', TabsController],
   controllerAs: 'tabsCtrl',
   bindings:{
     display: '='
   }
 };
 
-function TabsController($log, $location, $ducoment) {
+function TabsController($log, $location, $ducoment, $window) {
   $log.debug('TabsController');
 
+  this.show = false;
+  if($window.innerWidth > 450){
+    this.show = true;
+  }
   this.gotoAnchor = function(id){
     var section = angular.element(document.getElementById(id));
     $document.scrollTo(section, 30, 10000);
-  };
-
-  this.toTheTop = function() {
-    $document.scrollTop(30, 5000);
   };
 
 }
--- app/view/home/home-controller.js
+++ app/view/home/home-controller.js
@@ -2,13 +2,16 @@
 
 require('./_home.scss');
 
-module.exports = ['$log', '$location', '$rootScope', 'authService', homeController];
+module.exports = ['$log', '$location', '$document', homeController];
 
-function homeController($log, $location){
+function homeController($log, $location, $document){
   $log.debug('homeController');
 
   let url = $location.url();
   this.showHome = url === '/about' || '' || '/';
 
+  this.toTheTop = function() {
+    $document.scrollTop(30, 3000);
+  };
 
 }
