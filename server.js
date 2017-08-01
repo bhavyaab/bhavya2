95155768a7d7b82a39b23e6bff2134a226cb02b4
Bhavya Bharti <bhavyab@outlook.com>
2017-08-01 16:35:56 -0700

    added cors

--- server.js
+++ server.js
@@ -2,23 +2,25 @@
 
 const express = require('express');
 const dotenv = require('dotenv').load();
-// const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const cors = require('./lib/cors-middleware.js');
+const errors = require('./lib/error-middleware.js');
 const sendMail = require('./router/send-mail.js');
 
 const cors = function() {
   return function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+    res.header('Access-Control-Allow-Headers', '*');
     next();
   };
 };
-
-app.use(cors());
-app.use(sendMail);
 app.use(express.static(`${__dirname}/build`));
+
+app.use(cors);
+app.use(sendMail);
+app.use(errors); // always the last middleware
 
 app.get('/', (req, res) => {
   res.send('Welcome to my portfolio...');
